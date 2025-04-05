45393d2388fa2a9b8a06c658b31b07ade9048939
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2025-04-05 19:49:39 +0900

    style: apply pep8 formatting to main.py
    Apply pep8 formatting to improve code readability.

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ import dc_api
 
 import logging
 
-logging.basicConfig(level=logging.WARN,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.WARN, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 logger = logging.getLogger(__name__)
 
 
 class _AsyncTimedIterator:
 
-    __slots__ = ('_iterator', '_timeout', '_sentinel')
+    __slots__ = ("_iterator", "_timeout", "_sentinel")
 
     def __init__(self, iterable, timeout, sentinel):
         self._iterator = iterable.__aiter__()
@@ -32,7 +33,7 @@ class _AsyncTimedIterator:
 
 class AsyncTimedIterable:
 
-    __slots__ = ('_factory', )
+    __slots__ = ("_factory",)
 
     def __init__(self, iterable, timeout=None, sentinel=None):
         self._factory = lambda: _AsyncTimedIterator(iterable, timeout, sentinel)
@@ -42,9 +43,16 @@ class AsyncTimedIterable:
 
 
 async def send_telegram_message(context, message):
-    #bot_token = '12345:YOUR FULL BOT TOKEN'
-    #bot_chat_id = 'YOUR CHAR ID' #
-    url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + message
+    # bot_token = '12345:YOUR FULL BOT TOKEN'
+    # bot_chat_id = 'YOUR CHAR ID' #
+    url = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendMessage?chat_id="
+        + bot_chat_id
+        + "&parse_mode=Markdown&text="
+        + message
+    )
     response = requests.get(url)
     pprint.pprint(response)
 
@@ -58,35 +66,44 @@ async def run_loop(context):
 
     max_of_id = 0
     while True:
-        print('[INFO] Trying to fetch board messages...')
+        print("[INFO] Trying to fetch board messages...")
         index_generator = None
         if max_of_id != 0:
-            index_generator = api.board(context['board_id'], start_page=1, num=CONST_NUM_NORMAL_FETCH, document_id_lower_limit=max_of_id)
+            index_generator = api.board(
+                context["board_id"],
+                start_page=1,
+                num=CONST_NUM_NORMAL_FETCH,
+                document_id_lower_limit=max_of_id,
+            )
         else:
-            index_generator = api.board(context['board_id'], start_page=1, num=CONST_NUM_FIRST_FETCH)
-        print('[INFO] Done!')
-        message = ''
+            index_generator = api.board(
+                context["board_id"], start_page=1, num=CONST_NUM_FIRST_FETCH
+            )
+        print("[INFO] Done!")
+        message = ""
         cnt = 0
-        timed_index_generator = AsyncTimedIterable(iterable=index_generator, timeout=CONST_TIMEOUT_IN_SEC)
+        timed_index_generator = AsyncTimedIterable(
+            iterable=index_generator, timeout=CONST_TIMEOUT_IN_SEC
+        )
         async for index in timed_index_generator:
             if index:
-                print('[INFO] ... index.id ' + index.id)
+                print("[INFO] ... index.id " + index.id)
                 if int(index.id) > max_of_id:
                     max_of_id = int(index.id)
-                message += index.title + '\n'
+                message += index.title + "\n"
                 cnt += 1
         print(message)
         if cnt > 0:
             await send_telegram_message(context, message)
-        print('[INFO] now |max_of_id| is ' + str(max_of_id))
-        print('[INFO] Sleeping...')
+        print("[INFO] now |max_of_id| is " + str(max_of_id))
+        print("[INFO] Sleeping...")
         await asyncio.sleep(CONST_TIME_TO_SLEEP_IN_SEC)
-        print('[INFO] Done.')
+        print("[INFO] Done.")
 
 
 async def main():
     context = {}
-    context['board_id'] = 'smask'
+    context["board_id"] = "smask"
     await run_loop(context)
 
 
